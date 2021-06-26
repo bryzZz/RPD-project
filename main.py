f4d3d5b74e3c317aaf1c1d7faa93696a2da68af0
Nikita <tebloev.nikita@gmail.com>
2021-06-26 10:31:36 +0800

    upgrade prototype

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import eel, random
 
 eel.init('web') #инициализация папки в которой лежит приложение
 
+# @eel.expose
+# def getForm(componentsList):
+#     form = ''
+
+#     for i in range(len(componentsList)):
+#         formItem = '<div class="form__field {name}"></div><p class="{name}__text">{text}:</p><input type="{inputType}" name="{name}" tabindex="{i}" value="{inputValue}" placeholder="{placeholder}" required="{required}">'.format(
+#                 name = componentsList[i]['name'],
+#                 text = componentsList[i]['text'],
+#                 inputType = componentsList[i]['inputType'],
+#                 i = i,
+#                 inputValue = componentsList[i]['inputValue'],
+#                 placeholder = componentsList[i]['placeholder'],
+#                 required = componentsList[i]['required']
+#             )
+#         form += formItem
+    
+#     return form
+
 #
 # def print_(n):
 #     print('Got this from Javascript:', n)
--- web/js/index.js
+++ web/js/index.js
@@ -6,6 +6,65 @@
 //   return Math.random();
 // }
 
+function getForm(componentsList){
+    let form = '';
+
+    for(let i = 0; i < componentsList.length; i++){
+        let formItem = `
+        <div class="form__field ${componentsList[i]?.name}">
+            <p class="${componentsList[i]?.name}__text">${componentsList[i]?.text}:</p>
+            <input type="${componentsList[i]?.inputType}" name="${componentsList[i]?.name}" tabindex="${i}" value="${componentsList[i]?.inputValue}" placeholder="${componentsList[i]?.placeholder}" ${componentsList[i]?.required ? 'required' : ''}>
+        </div>
+        `;
+        form += formItem;
+    }
+
+    return form;
+}
+
+document.querySelector('.form__fields').innerHTML = getForm([
+    {
+        name: 'disciplineName',
+        text: 'Введите название дисциплины',
+        inputType: 'text',
+        inputValue: '',
+        placeholder: 'ЯРСК',
+        required: true
+    },
+    {
+        name: 'laborIntensity',
+        text: 'Введите трудоёмкость дисциплины',
+        inputType: 'number',
+        inputValue: '0',
+        placeholder: '0',
+        required: true
+    },
+    {
+        name: 'numberOfHoursAll',
+        text: 'Введите количество часов',
+        inputType: 'number',
+        inputValue: '0',
+        placeholder: '0',
+        required: true
+    },
+    {
+        name: 'examHours',
+        text: 'Введите количество часов на экзамен',
+        inputType: 'number',
+        inputValue: '0',
+        placeholder: '0',
+        required: true
+    },
+    {
+        name: 'finalExamination',
+        text: 'Введите форму итоговой аттестации',
+        inputType: 'text',
+        inputValue: 'Экз.',
+        placeholder: '',
+        required: true
+    }
+]) + document.querySelector('.form__fields').innerHTML;
+
 // событие которое сработает когда форма отправится
 document.querySelector('.form').addEventListener('submit', (e) => {
     e.preventDefault(); // отмена перезагрузки при отправке
