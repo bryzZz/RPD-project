e7e984ed73e46c1a22110fdd930bd0ed1a0ab2e8
Nikita <tebloev.nikita@gmail.com>
2021-06-30 21:17:48 +0800

    intermediate resut changes

--- src/js/intermediateResult.js
+++ src/js/intermediateResult.js
@@ -4,39 +4,57 @@ export default function intermediateResult(container, data){
     let inter = container.querySelector('.intermediateResultContainer') || createElement({tagName: 'div', className: 'intermediateResultContainer'});
     inter.innerHTML = '';
 
-    // const lineBreak = '\n';
-
-    inter.append(createElement({
-        tagName: 'h1',
-        textContent: 'IV. СОДЕРЖАНИЕ И СТРУКТУРА ДИСЦИПЛИНЫ\n\n',
-        styles: {
-            fontSize: '12pt',
-            fontWeight: 'bold'
-        }
-    }));
-    // container.append(lineBreak);
-    inter.append(createElement({
-        tagName: 'p',
-        textContent: `Трудоемкость дисциплины составляет ${data.laborIntensity} зачетных единиц, ${data.numberOfHoursAll} часов, ${data.examHours} часов на экзамен.\n\n`,
-    }));
-    inter.append(createElement({
-        tagName: 'p',
-        textContent: `Форма промежуточной аттестации: ${data.finalExamination}\n`,
-    }));
-
-    inter.append(createElement({
-        tagName: 'h2',
-        textContent: '4.1 Содержание дисциплины, структурированное по темам, c указанием видов учебных занятий и отведенного на них количества академических часов',
-        styles: {
-            fontSize: '12pt',
-            fontWeight: 'bold'
-        }
-    }));
+    //RPD IV header
+    inter.append(
+        createElement({
+            tagName: 'h1',
+            textContent: 'IV. СОДЕРЖАНИЕ И СТРУКТУРА ДИСЦИПЛИНЫ\n\n',
+            styles: {
+                fontSize: '12pt',
+                fontWeight: 'bold'
+            }
+        }),
+        createElement({
+            tagName: 'p',
+            textContent: `Трудоемкость дисциплины составляет ${data.laborIntensity} зачетных единиц, ${data.numberOfHoursAll} часов, ${data.examHours} часов на экзамен.\n\n`,
+        }),
+        createElement({
+            tagName: 'p',
+            textContent: `Форма промежуточной аттестации: ${data.finalExamination}\n`,
+        }),
+        createElement({
+            tagName: 'h2',
+            textContent: '4.1 Содержание дисциплины, структурированное по темам, c указанием видов учебных занятий и отведенного на них количества академических часов',
+            styles: {
+                fontSize: '12pt',
+                fontWeight: 'bold'
+            }
+        }),
+    );
 
     let table = document.createElement('table');
-		
+    
+    table.innerHTML += `
+        <tr>
+            <td rowspan="3">п/п</td>
+            <td rowspan="3">Раздел дисциплины/темы</td>
+            <td rowspan="3">Семестр</td>
+            <td colspan="4">Виды учебной работы, включая самостоятельную работу обучающихся и трудоемкость(в часах)</td>
+            <td rowspan="3">Формы текущего контроля успеваемости; Форма промежуточной аттестации(по семестрам)</td>
+        </tr>
+        <tr>
+            <td colspan="3">Контактная работа преподавателя с обучающимися</td>
+            <td rowspan="2">Самостоятельная работа</td>
+        </tr>
+        <tr>
+            <td>Лекции</td>
+            <td>Семинарские (практические занятия)</td>
+            <td>Консультации</td>
+        </tr>
+    `;
+
 	for (let i = 0; i < data.semesters.length; i++){
-        table.append(createRow([
+        table.append(createTableRow([
             'Семестр',
             i+1,
             data.semesters[i].lecturesHour || '',
@@ -46,7 +64,7 @@ export default function intermediateResult(container, data){
         ]));
 		
 		for (let j = 0; j < data.semesters[i].topics.length; j++){
-            table.append(createRow([
+            table.append(createTableRow([
                 data.semesters[i].topics[j].topicName,
                 i+1,
                 data.semesters[i].topics[j].lecturesHour || '',
@@ -56,7 +74,7 @@ export default function intermediateResult(container, data){
             ]));
 
 			for (let k = 0; k < data.semesters[i].topics[j].subtopics.length; k++){
-                table.append(createRow([
+                table.append(createTableRow([
                     data.semesters[i].topics[j].subtopics[k].subtopicName,
                     i+1,
                     data.semesters[i].topics[j].subtopics[k].lectureHour || '',
@@ -74,7 +92,7 @@ export default function intermediateResult(container, data){
     container.append(inter);
 }
 
-function createRow(cellList){
+function createTableRow(cellList){
     const row = document.createElement('tr');
     for(const cell of cellList){
         row.append(createElement({tagName: 'td', textContent: cell}));
