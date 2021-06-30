a9d696837da882718af50c9c41a9fa66fd8d5c27
Nikita <tebloev.nikita@gmail.com>
2021-06-30 22:36:15 +0800

    generate file changes

--- src/js/generateDocxFile.js
+++ src/js/generateDocxFile.js
@@ -1,29 +1,58 @@
 import { saveAs } from 'file-saver';
-import { Document, Packer, Paragraph, TextRun } from "docx";
+import { Document, Packer, Paragraph, TextRun, AlignmentType, Table, TableRow, TableCell } from "docx";
 
 export default function generateDocxFile(allData){
     const doc = new Document({
-        sections: [
-        {
+        sections: [{
             properties: {},
-            children: [
+            children: [		
+			new Paragraph({
+				spacing: {
+					after: 1,
+				},
+				alignment: AlignmentType.CENTER,
+				children: [
+					new TextRun({
+						text: "IV. СОДЕРЖАНИЕ И СТРУКТУРА ДИСЦИПЛИНЫ",
+						bold: true,
+						font: "Times",
+						size: 28,
+					}),
+				],
+			}),
             new Paragraph({
                 children: [
-                new TextRun("Hello World"),
                 new TextRun({
-                    text: "Foo Bar",
-                    bold: true
-                }),
-                new TextRun({
-                    text: "\tGithub is the best",
-                    bold: true
-                })
+					text: "Трудоёмкость дисциплины составляет " + `${allData.laborIntensity}` + " зачётных единиц, " + `${allData.numberOfHoursAll}` + " часов, " + `${allData.examHours}`  + " часов на экзамен.",
+					font: "Times New Roman",
+					size: 24,				
+				}),
                 ]
-            })
+            }),
+			new Paragraph({
+				children: [
+				new TextRun({
+					text: "Форма промежуточной аттестации: " + `${allData.finalExamination}`,
+					font: "Times New Roman",
+					size: 24,
+				}),
+				]
+			}),		
+			new Table({
+				rows: [
+					new TableRow({
+						children: [
+							new TableCell({
+								children: [],
+							})
+						]
+					})
+				]
+			})
             ]
-        }
-        ]
+        }]
     });
+
 
     console.log(doc);
     
