ac173e44f484295de8427eb07b53c99e4e0e5989
Bass0ff <86712541+Bass0ff@users.noreply.github.com>
2021-07-02 12:29:07 +0800

    Finished first table generation

--- src/js/generateDocxFile.js
+++ src/js/generateDocxFile.js
@@ -4,40 +4,69 @@ import {Document, Packer, Paragraph, TextRun, Header, AlignmentType, VerticalAli
 export default function generateDocxFile(allData){
 	
 	const tableRows = [];
+	let gen_lec = 0,
+		gen_sem = 0,
+		gen_con = 0,
+		gen_ind = 0;
+		
 	for (let i = 0; i < allData.semesters.length; i++){/*доделывается цикл*/
-		tableRows.push(new TableRow({
+		tableRows.push(new TableRow({ //Семетры
 			children: [
 				new TableCell({
 					children: [],
 				}),
 				new TableCell({
 					children: [
-						new Paragraph("Семестр"),
+						new Paragraph({
+							children:[
+								new TextRun({text: "Семестр", bold: true, font: "Times New Roman", size: 28}),
+							],
+						}),
 					]
 				}),
 				new TableCell({
 					children: [
-						new Paragraph(`${i+1}`),
+						new Paragraph({
+							children: [
+								new TextRun({text: `${i+1}`, bold: true, font: "Times New Roman", size: 28}),
+							],
+						}),
 					]
 				}),
 				new TableCell({
 					children: [
-						new Paragraph(`${allData.semesters[i].lecturesHour}`)
+						new Paragraph({
+							children: [
+								new TextRun({text: `${allData.semesters[i].lecturesHour}`, bold: true, font: "Times New Roman", size: 28}),
+							],
+						}),
 					]
 				}),
 				new TableCell({
 					children: [
-						new Paragraph(`${allData.semesters[i].seminarsHour}`)
+						new Paragraph({
+							children: [
+								new TextRun({text: `${allData.semesters[i].seminarsHour}`, bold: true, font: "Times New Roman", size: 28}), 
+							],
+						}),
 					]
 				}),
 				new TableCell({
 					children: [
-						new Paragraph(`${allData.semesters[i].consultationsHour}`)
+						new Paragraph({
+							children:[
+								new TextRun({text: `${allData.semesters[i].consultationsHour}`, bold: true, font: "Times New Roman", size: 28}),
+							],
+						}),
 					]
 				}),
 				new TableCell({
 					children: [
-						new Paragraph(`${allData.semesters[i].independentWorkHour}`)
+						new Paragraph({
+							children: [
+								new TextRun({text: `${allData.semesters[i].independentWorkHour}`, bold: true, font: "Times New Roman", size: 28}),
+							],
+						}),
 					]
 				}),
 				new TableCell({
@@ -45,43 +74,75 @@ export default function generateDocxFile(allData){
 				}),
 			]
 		}));
+		gen_lec += +allData.semesters[i].lecturesHour; console.log(gen_lec);
+		gen_sem += +allData.semesters[i].seminarsHour; console.log(gen_sem);
+		gen_con += +allData.semesters[i].consultationsHour; console.log(gen_con);
+		gen_ind += +allData.semesters[i].independentWorkHour; console.log(gen_ind);
 		for (let j = 0; j < allData.semesters[i].topics.length; j++){
-			tableRows.push(new TableRow({
+			tableRows.push(new TableRow({ //Разделы
 				children: [
 					new TableCell({
 						children: [
-							new Paragraph(`${j+1}`)
+							new Paragraph({
+								children: [
+									new TextRun({text: `${j+1}`, bold: true, font: "Times New Roman", size: 28}),
+								],
+							}),
 						],
 					}),
 					new TableCell({
 						children: [
-							new Paragraph(`${allData.semesters[i].topics[j].topicName}`),
+							new Paragraph({
+								children: [
+									new TextRun({text: `${allData.semesters[i].topics[j].topicName}`, bold: true, font: "Times New Roman", size: 28}),
+								],
+							}),	
+						],
+					}),
+					new TableCell({
+						children: [
+							new Paragraph({
+								children: [
+									new TextRun({text: `${i+1}`, bold: true, font: "Times New Roman", size: 28}),
+								],
+							}),
 						]
 					}),
 					new TableCell({
 						children: [
-							new Paragraph(`${i+1}`),
-						]
+							new Paragraph({
+								children: [
+									new TextRun({text: `${allData.semesters[i].topics[j].lecturesHour}`, bold: true, font: "Times New Roman", size: 28}),
+								],
+							}),
+						],
 					}),
 					new TableCell({
 						children: [
-							new Paragraph(`${allData.semesters[i].topics[j].lecturesHour}`)
-						]
+							new Paragraph({
+								children: [
+									new TextRun({text: `${allData.semesters[i].topics[j].seminarsHour}`, bold: true, font: "Times New Roman", size: 28}),
+								],
+							}),
+						],
 					}),
 					new TableCell({
 						children: [
-							new Paragraph(`${allData.semesters[i].topics[j].seminarsHour}`)
-						]
+							new Paragraph({
+								children: [
+									new TextRun({text: `${allData.semesters[i].topics[j].consultationsHour}`, bold: true, font: "Times New Roman", size: 28}),
+								],
+							}),
+						],
 					}),
 					new TableCell({
 						children: [
-							new Paragraph(`${allData.semesters[i].topics[j].consultationsHour}`)
-						]
-					}),
-					new TableCell({
-						children: [
-							new Paragraph(`${allData.semesters[i].topics[j].independentWorkHour}`)
-						]
+							new Paragraph({
+								children: [
+									new TextRun({text: `${allData.semesters[i].topics[j].independentWorkHour}`, bold: true, font: "Times New Roman", size: 28}),
+								],
+							}),
+						],
 					}),
 					new TableCell({
 						children: []
@@ -89,49 +150,79 @@ export default function generateDocxFile(allData){
 				]
 			})); 
 			for (let k = 0; k < allData.semesters[i].topics[j].subtopics.length; k++){
-				tableRows.push(new TableRow({
+				tableRows.push(new TableRow({ //Темы
 					children: [
 						new TableCell({
 							children: [
 								new Paragraph({
-								text: `${j+1}` + "." + `${k+1}`,
-								alignment: AlignmentType.CENTER,
+									alignment: AlignmentType.CENTER,
+									children: [
+										new TextRun({text: `${j+1}` + "." + `${k+1}`, font: "Times New Roman", size: 28}),
+									],
 								}),
 							],
 						}),
 						new TableCell({
 							children: [
-								new Paragraph(`${allData.semesters[i].topics[j].subtopics[k].subtopicName}`),
+								new Paragraph({
+									children: [
+										new TextRun({text: `${allData.semesters[i].topics[j].subtopics[k].subtopicName}`, font: "Times New Roman", size: 28}),
+									],
+								}),
 							],
 						}),
 						new TableCell({
 							children: [
-								new Paragraph(`${i+1}`),
+								new Paragraph({
+									children: [
+										new TextRun({text: `${i+1}`, font: "Times New Roman", size: 28}),
+									],
+								}),
 							],
 						}),
 						new TableCell({
 							children: [
-								new Paragraph(`${allData.semesters[i].topics[j].subtopics[k].lecturesHour}`),
+								new Paragraph({
+									children: [
+										new TextRun({text: `${allData.semesters[i].topics[j].subtopics[k].lecturesHour}`, font: "Times New Roman", size: 28}),
+									],
+								}),
 							],
 						}),
 						new TableCell({
 							children: [
-								new Paragraph(`${allData.semesters[i].topics[j].subtopics[k].seminarsHour}`),
+								new Paragraph({
+									children: [
+										new TextRun({text: `${allData.semesters[i].topics[j].subtopics[k].seminarsHour}`, font: "Times New Roman", size: 28}),
+									],
+								}),
 							],
 						}),
 						new TableCell({
 							children: [
-								new Paragraph(`${allData.semesters[i].topics[j].subtopics[k].consultationsHour}`),
+								new Paragraph({
+									children: [
+										new TextRun({text: `${allData.semesters[i].topics[j].subtopics[k].consultationsHour}`, font: "Times New Roman", size: 28}),
+									],
+								}),
 							],
 						}),
 						new TableCell({
 							children: [
-								new Paragraph(`${allData.semesters[i].topics[j].subtopics[k].independentWorkHour}`),
+								new Paragraph({
+									children: [
+										new TextRun({text: `${allData.semesters[i].topics[j].subtopics[k].independentWorkHour}`, font: "Times New Roman", size: 28}),
+									],
+								}),
 							],
 						}),
 						new TableCell({
 							children: [
-								new Paragraph(`${allData.semesters[i].topics[j].subtopics[k].formsOfMonitoringProgress}`),
+								new Paragraph({
+									children: [
+										new TextRun({text: `${allData.semesters[i].topics[j].subtopics[k].formsOfMonitoringProgress}`, font: "Times New Roman", size: 28}),
+									],
+								}),
 							]
 					}),
 				]
@@ -139,6 +230,69 @@ export default function generateDocxFile(allData){
 			}
 		}
 	}
+	tableRows.push(new TableRow({/*Строка "Итого"*/
+		children:[
+			new TableCell({
+				columnSpan: 2,
+				children: [
+					new Paragraph ({
+						children: [
+							new TextRun({text: "Итого часов:", bold: true, font: "Times New Roman", size: 28}),
+						],
+					}),
+							
+				],
+			}),
+			new TableCell({
+				children: [],
+			}),
+			new TableCell({
+				children: [
+					new Paragraph({
+						children: [
+							new TextRun({text: `${gen_lec}`, font: "Times New Roman", size: 28}),
+						],
+					}),
+				],
+			}),
+			new TableCell({
+				children: [
+					new Paragraph({
+						children: [
+							new TextRun({text: `${gen_sem}`, font: "Times New Roman", size: 28}),
+						],
+					}),
+				],
+			}),
+			new TableCell({
+				children: [
+					new Paragraph({
+						children: [
+							new TextRun({text: `${gen_con}`, font: "Times New Roman", size: 28}),
+						]
+					}),
+				],
+			}),
+			new TableCell({
+				children: [
+					new Paragraph({
+						children: [
+							new TextRun({text:`${gen_ind}`, size: 28, font: "Times New Roman"}),
+						],
+					}),
+				],
+			}),
+			new TableCell({
+				children: [
+					new Paragraph({
+						children: [
+							new TextRun({text: `${allData.examHours}`, font: "Times New Roman", size: 28}),
+						],
+					}),
+				],
+			}),
+		]
+	}))
 	let table = new Table({
 		width: {
 			size: 90,
