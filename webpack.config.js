bd930efe37d027f1179c1a3a34826bf1a50db2bc
Nikita <tebloev.nikita@gmail.com>
2021-07-07 23:24:12 +0900

    webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,79 @@
-const path = require("path")
-const HTMLPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path"),
+    HTMLPlugin = require("html-webpack-plugin"),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+    { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    TerserPlugin = require('terser-webpack-plugin');
+
+// variables to know which mode we are in
+const isDev = process.env.NODE_ENV === 'development', isProd = !isDev;
+
+// function for correct names
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const plugins = () => {
+    return [
+        new CleanWebpackPlugin(),
+        new HTMLPlugin({
+            template: "./index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new MiniCssExtractPlugin({ filename: "css/" + filename('css')}),
+    ];
+}
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+  
+    if (isProd) {
+        config.minimize = true;
+        config.minimizer = [new TerserPlugin()]
+    }
+  
+    return config
+}
 
 module.exports = {
-  entry: ["./src/js/index.js"],
-  output: {
-    filename: "js/[name].js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "",
-  },
-  plugins: [
-    new HTMLPlugin({ template: "./src/index.html" }),
-    new MiniCssExtractPlugin({ filename: "css/[name].css" }),
-    new CleanWebpackPlugin(),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, "dist/index.html"),
-    open: true,
-    compress: true,
-    hot: true,
-    port: 3000,
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(s[ac]ss|css)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: (resourcePath, context) => {
-                return path.relative(path.dirname(resourcePath), context) + "/";
-              },
+    context: path.resolve(__dirname, 'src'),
+    entry: ["./js/index.js"],
+    output: {
+        filename: 'js/' + filename('js'),
+        path: path.resolve(__dirname, "dist"),
+    },
+    optimization: optimization(),
+    plugins: plugins(),
+    devServer: {
+        historyApiFallback: isDev,
+        contentBase: path.resolve(__dirname, "dist/index.html"),
+        open: isDev,
+        compress: isDev,
+        hot: isDev,
+        port: 3000,
+    },
+    devtool: isDev ? 'source-map' : false,
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]ss|css)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                        publicPath: (resourcePath, context) => {
+                            return path.relative(path.dirname(resourcePath), context) + "/";
+                        },
+                        },
+                    },
+                    "css-loader",
+                    "sass-loader",
+                ],
             },
-          },
-          "css-loader",
-          "sass-loader",
         ],
-      },
-    ],
-  },
+    },
 };
